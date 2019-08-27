ad6a96d3dc05cec20a32fbe2f4851dca6ca7b93e
Ruturaj Kadikar <ruturaj@infracloud.io>
2019-08-27 14:20:26 +0530

    Update run-script.sh

--- consul-client/scripts/run-script.sh
+++ consul-client/scripts/run-script.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#consul agent --config-dir=/consul/config -retry-join myconsul-consul-server.default.svc.cluster.local   // This command can also be used for joining to Consul Server
-consul agent --config-dir=/consul/config -retry-join "provider=k8s label_selector=\"app=consul,component=server\""
+consul agent --config-dir=/consul/config -retry-join myconsul-consul-server.default.svc.cluster.local
